68f229d869507d37538265158cddf323b20480e2
Jan Tuomi <jan.tuomi@eficode.com>
2018-02-27 17:42:05 +0200

    Refactor stuff

--- index.js
+++ index.js
@@ -1,141 +1,20 @@
 require('dotenv').config();
 
-const fs = require('fs');
 const path = require('path');
 const util = require('util');
 const models = require('./models');
+const parser = require('./parser');
+const watcher = require('./watcher');
+const utils = require('./utils');
 const Koa = require('koa');
 const Router = require('koa-router');
 const limit = require('koa-limit');
-var mm = require('musicmetadata');
 
 const app = new Koa();
 const router = new Router();
 
-
-const dir = process.env.MUSIC_DIRECTORY;
-
-const saveMetadataToCollection = async (data, pathToFile) => {
-  const query = {
-    title: data.title,
-    album: data.album,
-  };
-
-  const { albumartist, ...value } = 
-  await models.library.update(query, {
-    ...data,
-    filePath: pathToFile,
-  }, { upsert: true });
-}
-
-const parseAndSaveMetadata = async p => {
-  const existing = await models.library.findOne({ filePath: p });
-  if (existing) {
-    console.log(`File "${p}" already in DB. Skipping...`);
-    return;
-  }
-
-  const parser = mm(fs.createReadStream(p), (err, metadata) => {
-    console.log(`Parsing data from: ${p}`)
-    if (err) {
-      console.log(`Error parsing file ${p}. Skipping...`);
-      return;
-    }
-    
-    const { albumartist, ...withoutAlbumArtist } = metadata;
-    const data = { 
-      ...withoutAlbumArtist,
-      artist: [].concat(metadata.artist || [], metadata.albumartist || []),
-    };
-  
-    saveMetadataToCollection(data, p);
-  });
-}
-
-const parseSong = async song => {
-  if (!fs.lstatSync(song).isFile()) {
-    console.log(`Not a file: ${song}. Skipping...`);
-    return;
-  }
-
-  parseAndSaveMetadata(`${song}`);
-}
-
-const traverseAlbumDir = async dir => {
-  if (!fs.lstatSync(dir).isDirectory()) {
-    console.log(`Not a directory: ${dir}. Skipping...`);
-    return;
-  }
-
-  const songs = fs.readdirSync(dir).map(d => `${dir}/${d}`);
-
-  for (song of songs) {
-    parseSong(song);
-  }
-}
-
-const traverseArtistDir = async dir => {
-  if (!fs.lstatSync(dir).isDirectory()) {
-    console.log(`Not a directory: ${dir}. Skipping...`);
-    return;
-  }
-
-  const albumDirs = fs.readdirSync(dir).map(d => `${dir}/${d}`);
-
-  for (albumDir of albumDirs) {
-    traverseAlbumDir(albumDir);
-  }
-}
-
-const traverseDirectoryAndSave = async p => {
-  if (!fs.lstatSync(p).isDirectory()) {
-    throw new Error(`Not a directory: ${p}`);
-  }
-
-  try {
-    const artistDirs = fs.readdirSync(p).map(d => `${p}/${d}`);
-
-    for (artistDir of artistDirs) {
-      traverseArtistDir(artistDir);
-    }
-    
-  } catch (err) {
-    console.log('Error traversing directory.')
-    console.log(err);
-  }
-};
-
-traverseDirectoryAndSave(dir);
-
-
-fs.watch(dir, { recursive: true }, function (event, filename) {
-  if (filename) {
-      console.log('filename provided: ' + filename);
-      const fullPath = `${dir}/${filename}`;
-      if (fs.existsSync(fullPath)) {
-        console.log('file created or modified.');
-      } else {
-        console.log('file removed.');
-      }
-
-      const parts = filename.split('/');
-      const depth = parts.length - 1;
-
-      if (depth === 0) {
-        console.log(`Refreshing artist directory "${fullPath}"...`);
-        traverseArtistDir(fullPath);
-      } else if (depth === 1) {
-        console.log(`Refreshing album directory "${fullPath}"...`);
-        traverseAlbumDir(fullPath);
-      } else if (depth === 2) {
-        console.log(`Refreshing song "${fullPath}"...`);
-        parseSong(fullPath);
-      }
-
-  } else {
-      console.log('filename not provided');
-  }
-});
+parser.run();
+watcher();
 
 const stripFields = data => {
   return {
@@ -152,7 +31,7 @@ router.get('/', async ctx => {
 
 router.get('/artist/:artist', async ctx => {
   const artist = ctx.params.artist;
-  console.log(`Querying for artist "${artist}".`)
+  utils.info(`Querying for artist "${artist}".`)
   const data = await models.library.findOne({
     artist,
   });
@@ -161,7 +40,7 @@ router.get('/artist/:artist', async ctx => {
 
 router.get('/title/:title', async ctx => {
   const title = ctx.params.title;
-  console.log(`Querying for title "${title}".`)
+  utils.info(`Querying for title "${title}".`)
   const data = await models.library.findOne({
     title,
   });
@@ -170,11 +49,31 @@ router.get('/title/:title', async ctx => {
 
 router.get('/album/:album', async ctx => {
   const album = ctx.params.album;
-  console.log(`Querying for album "${album}".`)
+  utils.info(`Querying for album "${album}".`)
   const data = await models.library.findOne({
     album,
   });
   ctx.body = JSON.stringify(stripFields(data));
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  if (ctx.status < 400) {
+    utils.info(`${ctx.status} ${ctx.method} ${ctx.url} - ${ms}ms`);
+  } else {
+    utils.error(`${ctx.status} ${ctx.method} ${ctx.url} - ${ms}ms`);
+  }
 });
 
 app
---
+++ parser.js
@@ -0,0 +1,107 @@
+var mm = require('musicmetadata');
+const fs = require('fs');
+
+const models = require('./models');
+const utils = require('./utils');
+
+const dir = process.env.MUSIC_DIRECTORY;
+
+const saveMetadataToCollection = async (data, pathToFile) => {
+  const query = {
+    title: data.title,
+    album: data.album,
+  };
+
+  const { albumartist, ...value } = 
+  await models.library.update(query, {
+    ...data,
+    filePath: pathToFile,
+  }, { upsert: true });
+}
+
+const parseAndSaveMetadata = async p => {
+  const existing = await models.library.findOne({ filePath: p });
+  if (existing) {
+    utils.info(`File "${p}" already in DB. Skipping...`);
+    return;
+  }
+
+  const parser = mm(fs.createReadStream(p), (err, metadata) => {
+    utils.info(`Parsing data from: ${p}`)
+    if (err) {
+      utils.warning(`Error parsing file ${p}. Skipping...`);
+      return;
+    }
+    
+    const { albumartist, ...withoutAlbumArtist } = metadata;
+    const data = { 
+      ...withoutAlbumArtist,
+      artist: [].concat(metadata.artist || [], metadata.albumartist || []),
+    };
+  
+    saveMetadataToCollection(data, p);
+  });
+}
+
+const parseSong = async song => {
+  if (!fs.lstatSync(song).isFile()) {
+    utils.warning(`Not a file: ${song}. Skipping...`);
+    return;
+  }
+
+  parseAndSaveMetadata(`${song}`);
+}
+
+const traverseAlbumDir = async dir => {
+  if (!fs.lstatSync(dir).isDirectory()) {
+    utils.warning(`Not a directory: ${dir}. Skipping...`);
+    return;
+  }
+
+  const songs = fs.readdirSync(dir).map(d => `${dir}/${d}`);
+
+  for (song of songs) {
+    parseSong(song);
+  }
+}
+
+const traverseArtistDir = async dir => {
+  if (!fs.lstatSync(dir).isDirectory()) {
+    utils.warning(`Not a directory: ${dir}. Skipping...`);
+    return;
+  }
+
+  const albumDirs = fs.readdirSync(dir).map(d => `${dir}/${d}`);
+
+  for (albumDir of albumDirs) {
+    traverseAlbumDir(albumDir);
+  }
+}
+
+const traverseDirectoryAndSave = async p => {
+  if (!fs.lstatSync(p).isDirectory()) {
+    throw new Error(`Not a directory: ${p}`);
+  }
+
+  try {
+    const artistDirs = fs.readdirSync(p).map(d => `${p}/${d}`);
+
+    for (artistDir of artistDirs) {
+      traverseArtistDir(artistDir);
+    }    
+  } catch (err) {
+    utils.error('Error traversing directory.')
+    utils.error(err);
+  }
+};
+
+const run = () => {
+  traverseDirectoryAndSave(dir);
+}
+
+module.exports = {
+  run,
+  traverseAlbumDir,
+  traverseArtistDir,
+  parseSong,
+};
